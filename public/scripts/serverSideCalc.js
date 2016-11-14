599876acdeec073090bea66a0102320d811cb345
devjanaprime <dev@primeacademy.io>
2016-11-14 15:03:05 -0600

    updated for hard and pro mode

--- public/scripts/serverSideCalc.js
+++ public/scripts/serverSideCalc.js
@@ -1,13 +1,17 @@
 var verbose = true;
+var x = '';
+var y = '';
+var type ='';
+var capturingY = false;
 
 $( document ).ready( function(){
     $( '#calculateButton' ).on( 'click', function(){
       if( verbose ) console.log( 'calculateButton clicked' );
       // assemble user input into an object to send
       var objectToSend = {
-        x: $( '#xIn' ).val(),
-        y: $( '#yIn' ).val(),
-        type: $( '#typeIn' ).val()
+        x: x,
+        y: y,
+        type: type
       }; // end objectToSend
       if( verbose ) console.log( 'sending:', objectToSend );
       // POST to server for calculation
@@ -25,13 +29,37 @@ $( document ).ready( function(){
       }); // end ajax
     }); // end calculateButton on click
 
-    $( '#clearButton').on( 'click', function(){
-      // clear inputs
-      $( '#xIn' ).val('');
-      $( '#yIn' ).val('');
-      // reset type selector
-      $( '#typeIn').val('add');
+    $( '#clearButton' ).on( 'click', function(){
       // clear output
       $( '#output').html('');
+      // clear global vars
+      x = '';
+      y = '';
+      // reset global variables
+      type ='';
+      capturingY = false;
     }); // end clearButton on click
+
+    $( '.numberIn' ).on( 'click', function(){
+      console.log( 'numberIn class clicked:', $( this ).attr( 'number' ) );
+      if( capturingY ){
+        y += $( this ).attr( 'number' );
+        $( '#output' ).html( y );
+      }
+      else{
+        x += $( this ).attr( 'number' );
+        $( '#output' ).html( x );
+      }
+    }); // end numberIn on click
+
+    $( '.typeIn' ).on( 'click', function(){
+      console.log( 'typeIn on click:', $( this ).attr( 'type' ) );
+      // set type = the type attribute of this button
+      type = $( this ).attr( 'type' );
+      // start capturingY if not already capturing y
+      if( !capturingY ){
+        capturingY = true;
+        console.log( 'flipping over to capture y');
+      }
+    }); // end typeIn on click
 }); // end doc reacy
--- readme.md
+++ readme.md
@@ -1,10 +1,25 @@
 Weekend Challenge 3 Live Solve
 ==============================
 
+Normal Mode
+-----------
+
 [x] client: user inputs (x, y, type)
 [x] client: assemble an object with user inputs
 [x] client: send object via POST call to server
 [x] server: determine operator and do math
 [x] server: return object with an answer
 [x] client: interpret answer and display on DOM
-[] client: clear button, duh....
+[x] client: clear button, duh....
+
+Hard Mode
+---------
+
+[] physical calculator-like interface
+[] input first number from virtual calculator
+[] select operator from virtual calculator
+[] input second number from virtual calculator
+[] click "=" to set off calculation 
+[] update object to send
+[] update output display
+[] update clear
